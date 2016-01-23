dcd4d89ab10fec2e1ec71bb01c6afa91e4a3f7bc
Andrew Klimek <andrew.klimek@gmail.com>
2016-01-23 18:29:34 -0500

    submenu styling and minor layout fixes

--- 404.php
+++ 404.php
@@ -9,7 +9,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<!-- <div id="primary" class="content-area"> -->
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
@@ -58,7 +58,7 @@ get_header(); ?>
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
-	</div><!-- #primary -->
+	<!-- </div> --><!-- #primary -->
 
 <?php
 get_footer();

2a23c3b1adb5abca772f49d1a68814263311460d
Andrew J Klimek <andrew.klimek@gmail.com>
2016-03-30 21:43:07 -0400

    child theme example remove page headers

--- frenchpress-child/functions.php
+++ frenchpress-child/functions.php
@@ -13,6 +13,8 @@ add_action( 'wp_enqueue_scripts', function() {
 	// wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ), filemtime( get_stylesheet_directory() . '/style.css'), 'screen and (max-width: 900px)' );
 });
 
+// Don't display title on pages
+add_filter( 'frenchpress_page_titles', '__return_false' );
 
 // No "posted on" line
 // function frenchpress_posted_on() { return; }

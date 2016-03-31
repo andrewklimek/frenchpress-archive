7fb245e7be2508e3810d56002c35596cf989bf6e
Andrew J Klimek <andrew.klimek@gmail.com>
2016-03-30 21:44:08 -0400

    add hook to remove page headers

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,9 +10,12 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+	<?php if ( apply_filters( 'frenchpress_page_titles', true ) ) : ?>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
+	<?php endif; ?>
 
 	<div class="entry-content">
 		<?php

047814d022c3079f4722d14459f31c886fbd4057
Andrew J Klimek <andrew.klimek@gmail.com>
2016-04-09 02:37:28 -0400

    no # in custom background defaults

--- functions.php
+++ functions.php
@@ -75,7 +75,7 @@ function frenchpress_setup() {
 
 	// Set up the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'frenchpress_custom_background_args', array(
-		'default-color' => '#FFFDF8',
+		'default-color' => 'FFFDF8',
 		'default-image' => '',
 	) ) );
 }

f306803c7fcaa7d1a712b7861e96f596cbbe7b64
Andrew <andrew@readycat.net>
2016-09-23 06:44:14 +0000

    removed <a> from site title on home page and <p> elsewhere

--- header.php
+++ header.php
@@ -76,9 +76,9 @@ wp_head();
 					<?php
 				endif;
 				if ( is_front_page() || is_home() ) : ?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
 				<?php else : ?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<a class="site-title" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 				<?php
 				endif;
 					$description = get_bloginfo( 'description', 'display' );

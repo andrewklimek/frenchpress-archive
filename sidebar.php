d1dde653b15f80cd35731184b2790dfe812bbd54
Andrew <andrew@readycat.net>
2016-09-23 00:10:46 +0000

    filter sidebar, odd css tweaks from sanitize.css

--- sidebar.php
+++ sidebar.php
@@ -7,13 +7,16 @@
  * @package FrenchPress
  */
 
-if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-<aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside>
-<?php endif;
-if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
-<aside id="tertiary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-2' ); ?>
-</aside>
-<?php endif;
+if ( apply_filters( 'frenchpress_sidebar', true ) ) {
+
+	if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+	<aside id="secondary" class="widget-area">
+		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	</aside>
+	<?php endif;
+	if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+	<aside id="tertiary" class="widget-area">
+		<?php dynamic_sidebar( 'sidebar-2' ); ?>
+	</aside>
+	<?php endif;
+}

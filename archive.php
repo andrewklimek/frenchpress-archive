7d277887bde9fe44b1192da18252e5cdf59ee676
Andrew Klimek <andrew.klimek@gmail.com>
2017-06-19 17:24:28 -0400

    add cutom post type to dynamic content template on archives

--- archive.php
+++ archive.php
@@ -23,12 +23,13 @@ if ( have_posts() ) :
 	/* Start the Loop */
 	while ( have_posts() ) : the_post();
 
-		/*
-		 * Include the Post-Format-specific template for the content.
-		 * If you want to override this in a child theme, then include a file
-		 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-		 */
-		get_template_part( 'template-parts/content', get_post_format() );
+        /* specific content templates can be made like: content[-custom post type][-post format].php */
+		$name = get_post_type();
+		if ( 'post' === $name ) $name = '';
+		$format = get_post_format();
+		if ( $format ) $name = $name ? "{$name}-{$format}" : $format;
+
+		get_template_part( 'template-parts/content', $name );
 
 	endwhile;
 

04195fca558058a6b4735fce19f4b2c862261c29
Andrew Klimek <andrew.klimek@gmail.com>
2017-06-27 13:59:09 -0400

    take anonymous function out of for loop

--- js/nav.js
+++ js/nav.js
@@ -84,21 +84,18 @@
 	
 	/**
 	 * finds all links on the page which start with # and sets up smooth scrolling for them
-	 *  use an href of just "#" for "sroll to top" functionality
+	 * use an href of just "#" for "sroll to top" functionality
 	 */
+    function onPageLinkScroll(e){
+    	var target = 0;
+    	if ( this.hash ) {// href="#" has empty hash so target stays 0 (top of page)
+    		target = document.querySelector(this.hash).offsetTop;
+    	}
+    	mnscrl(target);
+    	e.preventDefault();
+    }
     for ( var a = document.querySelectorAll('a[href^="#"]'), i=0; i<a.length; ++i ) {
-    	a[i].addEventListener('click', function(e){
-    	    
-    	    if ( document.body.classList.contains( 'mobile-nav-open' ) ) { 
-    	        toggleMenu();// close menu drawer if it was open
-    	    }
-    		var target = 0;
-    		if ( this.hash ) {// href="#" has empty hash so target stays 0 (top of page)
-    		    target = document.querySelector(this.hash).offsetTop;
-    		}
-    		mnscrl(target);
-    		e.preventDefault();
-    	});
+    	a[i].addEventListener('click', onPageLinkScroll );
     }
 })();
 

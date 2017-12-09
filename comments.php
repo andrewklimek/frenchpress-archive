851b37f4d9aa75f9d85f1d934b1451632be8623a
Andrew Klimek <andrew.klimek@gmail.com>
2017-12-09 15:45:29 -0500

    more whitespace tweaks

--- comments.php
+++ comments.php
@@ -18,34 +18,29 @@
 if ( post_password_required() ) {
 	return;
 }
-?>
 
-<section id="comments" class="comments-area">
-	<?php
+echo '<section id="comments" class="comments-area">';
 	
 	if ( have_comments() ) :
 		
-		?>
-		<h2 class="comments-title h3">
-			<?php
+		echo '<h2 class="comments-title h3">';
+			
 			$comments_number = get_comments_number();
 			if ( '1' === $comments_number ) {
 				_e( 'One Comment', 'frenchpress' );
 			} else {
 				printf( __( '%1$s Comments', 'frenchpress' ), $comments_number );
 			}
-			?>
-		</h2>
-		<ol class="comment-list">
-			<?php
+		echo '</h2>
+			<ol class="comment-list">';
+		
 				wp_list_comments( array(
 					'callback'   => 'frenchpress_comment',// this is in inc/template-tags.php
 					'style'      => 'ol',
 					'short_ping' => true,
 				) );
-			?>
-		</ol>
-		<?php 
+				
+		echo '</ol>';
 		
 		the_comments_pagination( // array(
 					// 'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous', 'twentyseventeen' ) . '</span>',
@@ -57,13 +52,12 @@ if ( post_password_required() ) {
 
 
 	// If comments are closed and there are comments, let's leave a little note, shall we?
-	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) {
 
-		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'frenchpress' ); ?></p>
-	<?php
-	endif;
+		echo '<p class="no-comments">' . __( 'Comments are closed.', 'frenchpress' ) . '</p>';
+	}
 
 	comment_form();
-	?>
-
-</section>
+	
+	echo '</section>';
+	

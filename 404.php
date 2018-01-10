aa032ca9295bc4a3c1fb989bcfa174ddbcf775ac
Andrew Klimek <andrew.klimek@gmail.com>
2018-01-10 01:44:35 -0500

    new filter: frenchpress_replace_smart_404

--- 404.php
+++ 404.php
@@ -10,61 +10,64 @@
 get_header();
 ?>
 <main id="primary" class="site-main fffi fffi-99">
-		<article class="error-404 not-found">
-			<?php
-			if ( ! apply_filters( 'frenchpress_title_in_header', false ) ) :
-			?>
-			<header class="page-header">
-				<h1 class="title"><?php esc_html_e( '404', 'frenchpress' ); ?></h1>
-			</header>
-			<?php
-			endif;
-			?>
-			<div class="page-content">
-				<p><?php
+	<article class="error-404 not-found">
+		<?php
+		
+		if ( ! apply_filters( 'frenchpress_title_in_header', false ) ) {
+		
+			echo '<header class="page-header"><h1 class="title">404</h1></header>';
+		
+		}
+		
+		echo '<div class="page-content">';
+          
+		if ( $custom_404 = apply_filters( 'frenchpress_replace_smart_404', false ) ) :
+
+			echo $custom_404 === true ? '' : $custom_404;
+
+		else :
+			
+			echo '<p>';
+		
+			if ( isset( $_SERVER['HTTP_REFERER'] ) ) {
+
+				echo __( 'Unfortunately, you clicked a broken link.  Please try searching below.', 'frenchpress' );
+
+			} else {
+			
+				echo esc_html( "“{$_SERVER['REQUEST_URI']}”" ) . __( ' does not exist on this site. Please double-check the spelling or search below.', 'frenchpress' );
+			
+			}
+		
+			get_search_form();
+	
+			$request = $GLOBALS['wp_query']->query;
+			// $request = explode( '/', parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH ) );
+			$search = preg_replace('/\W+/', '+', urldecode( end( $request ) ) );
+	
+			$query = new WP_Query( array( 's' => $search, 'posts_per_page' => 10, 'nopaging' => true ) );
+
+			if ( $query->have_posts() ) {
+
+				echo '<h3>' . __( 'Perhaps you were looking for one of these:', 'frenchpress' ) . '</h3>';
+			
+				while ( $query->have_posts() ) {
 					
-					if ( isset( $_SERVER['HTTP_REFERER'] ) ) {
-						
+					$query->the_post();
 
-						esc_html_e( 'Unfortunately, you clicked a broken link.  Please try searching below.', 'frenchpress' );
+					get_template_part( 'template-parts/content', 'search' );
 
-					} else {
-						
-						echo esc_html( "“{$_SERVER['REQUEST_URI']}”" );
-						esc_html_e( ' does not exist on this site. Please double-check the spelling or search below.', 'frenchpress' );
-						
-					}
-					
-					get_search_form();
-				
-					$request = $GLOBALS['wp_query']->query;
-					// $request = explode( '/', parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH ) );
-					$search = preg_replace('/\W+/', '+', urldecode( end( $request ) ) );
-				
-					$query = new WP_Query( array( 's' => $search, 'posts_per_page' => 10, 'nopaging' => true ) );
+				}
 
-					if ( $query->have_posts() ) : ?>
-
-					<h3><?php esc_html_e( 'Perhaps you were looking for one of these:', 'frenchpress' ); ?></h3>
-
-						<?php
-						/* Start the Loop */
-						while ( $query->have_posts() ) : $query->the_post();
-
-						get_template_part( 'template-parts/content', 'search' );
-
-					endwhile;
-
-					// the_posts_navigation();
-
-					// else :
-
-						wp_reset_postdata();
-					
-					endif;
-				?>
-			</div>
-		</article>
+				wp_reset_postdata();
+		
+			}
+			
+		endif;
+		
+		?>
+		</div>
+	</article>
 <?php
 
 do_action('frenchpress_main_bottom');

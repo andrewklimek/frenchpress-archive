8cd8bf2f4ae8ba687d463797ac4ae9ece139ead8
Andrew Klimek <andrew.klimek@gmail.com>
2019-05-22 14:20:29 -0400

    minor disembellish update

--- inc/disembellish.php
+++ inc/disembellish.php
@@ -2,7 +2,7 @@
 /**
  * Disable various core embellishments you may not want (emoji, capital P, archive type in page title)
  *
- * Also a plugin, so versioning: 1.4.3
+ * Also a plugin, so versioning: 1.4.4
  */
 
 
@@ -59,7 +59,7 @@ add_filter('wp_mail_from_name', function($name){
  * Disable capital P
  */
 
-foreach ( array( 'the_content', 'the_title', 'wp_title', 'comment_text' ) as $filter ) {
+foreach ( array( 'the_content', 'the_title', 'wp_title', 'comment_text', 'widget_text_content' ) as $filter ) {
 	$priority = has_filter( $filter, 'capital_P_dangit' );
 	if ( $priority !== FALSE ) {
 		remove_filter( $filter, 'capital_P_dangit', $priority );
@@ -71,6 +71,7 @@ foreach ( array( 'the_content', 'the_title', 'wp_title', 'comment_text' ) as $fi
  */
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'embed_head', 'print_emoji_detection_script' );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
